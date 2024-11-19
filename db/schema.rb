a4af0e417dd3a58b71039dcb323cd6de1ab14fd0
Renee Bibby <renee.s.bibby@gmail.com>
2024-11-19 19:01:38 +1100

    more geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_15_074045) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_16_004236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_15_074045) do
     t.string "nickname"
     t.float "latitude"
     t.float "longitude"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
