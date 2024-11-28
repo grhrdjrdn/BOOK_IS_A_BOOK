63edadf9d54df6512c24654e801899c5141e00bf
min liu <heather81472@gmail.com>
2024-11-28 15:51:24 +0800

    add authorization and improve chat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit::Authorization
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -20,5 +21,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,8 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all
-
+    # @books = Book.all
+    @books = policy_scope(Book)
     owners = @books.map do |book|
       book.current_owner
     end
@@ -25,14 +25,17 @@ class BooksController < ApplicationController
     @request = Request.new
     @requests = @book.requests.where(user: current_user)
     @requests_on_this_book = Request.joins(:user).where(book: @book, user: current_user)
+    authorize @book
   end
 
   def new
     @book = Book.new
+    authorize @book
   end
 
   def create
     @book = Book.new(book_params)
+    authorize @book
     History.create(date_acquired: Date.today, book: @book, user: current_user)
     if @book.save
       redirect_to @book, notice: "Book was successfully created."
@@ -42,9 +45,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+    authorize @book
   end
 
   def update
+    authorize @book
     if @book.update(book_params)
       redirect_to @book, notice: "Book was successfully updated."
     else
