cd734013b867f6f901fa918b8b1d3208e1952d02
Gerhard Jordan <mail@gerhardjordan.com>
2024-12-03 19:54:57 +1100

    added user sign up location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :nickname, :location])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :nickname, :location])
   end
 
   # Pundit: allow-list approach
